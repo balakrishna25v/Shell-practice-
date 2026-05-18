49ee1a499104c03e31d0bd2c1bd5986ba3df9dab
balu <balu.v25@gmail.com>
2026-05-19 00:06:45 +0530

    action

--- 02-conver.sh
+++ 02-conver.sh
@@ -3,4 +3,5 @@
 echo "suresh:: hello ramesh, how are you doing?"
 echo "ramesh:: i am doing good, how about you?"
 echo "suresh:: i am doing good too, thanks for asking."
-    
+echo "ramesh:: will have coffee"
+        
